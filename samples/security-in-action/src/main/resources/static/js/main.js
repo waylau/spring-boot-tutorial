1bfcacb2476db8f3d97fe8c2a8988751a8295726
waylau <waylau521@gmail.com>
2017-03-09 17:39:07 +0800

    uipdate

---
+++ samples/security-in-action/src/main/resources/static/js/main.js
@@ -0,0 +1,16 @@
+/**
+ * Bolg main JS.
+ * Created by waylau.com on 2017/3/9.
+ */
+"use strict";
+//# sourceURL=main.js
+
+// DOM 加载完再执行
+$(function() {
+
+	// nav bar 点击样式
+    $('#navbarsContainer a').click(function (e) {
+        $(this).tab('show')
+    })
+
+});
