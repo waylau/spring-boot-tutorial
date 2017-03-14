fed0d73b4e619efe71619947ac682f8dc79b84d7
waylau <waylau521@gmail.com>
2017-03-14 23:25:39 +0800

    update

--- samples/security-stateless-in-action/src/main/resources/static/js/main.js
+++ samples/security-stateless-in-action/src/main/resources/static/js/main.js
@@ -32,4 +32,34 @@ $(function() {
 	    });
 	});
 	
+//	$('#loginform').submit(function(e) {
+//		  e.preventDefault()
+//
+//		  $.ajax({
+//		    url: `http://localhost:3000/users/login`,
+//		    type: 'POST',
+//		    data: $(this).serialize(),
+//		    dataType: 'json',
+//		    success: function(data) {
+//		      if (data) {
+//		        if (data.usernotfound) {
+//		          sweetAlert('Oops...', 'user not found!', 'error')
+//		        }
+//		        if (data.passerror) {
+//		          sweetAlert('Oops...', 'Password wrong!', 'error')
+//		        }
+//		        if (data.token) {
+//		          let token = data.token
+//		          localStorage.setItem('token', token)
+//
+//		          window.location.href = 'http://127.0.0.1:8080/home.html'
+//		        }
+//		      }
+//		    },
+//		    error: function(err) {
+//		      console.log(err)
+//		    }
+//
+//		  })
+//		})
 });
